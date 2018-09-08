b32a8fa430b0115da1c6a90d68b717b989c3a2e9
elfsong <dumingzhe@126.com>
2018-09-09 03:22:59 +1000

    Url jump debug

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, url_for
 
 app = Flask(__name__, static_url_path='', root_path='/root/SPM')    
 
