92842647a710697a656924e45ba65ead3e96748a
elfsong <dumingzhe@126.com>
2018-09-08 15:43:03 +0000

    version 0.0.1

---
+++ demo.py
@@ -0,0 +1,24 @@
+from flask import Flask,request,render_template,redirect
+
+app = Flask(__name__, static_url_path='', root_path='/root/SPM')    
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username =="user" and password=="password":
+            return redirect("http://www.google.com")
+        else:
+            message = "Failed Login"
+            return render_template('login.html',message=message)
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
